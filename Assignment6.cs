8e71e5173f4f8efdf8c9d303387c680c53494291
x10339966 <patbirkeland@mac.com>
2016-08-15 16:32:43 -0700

    Assignment6

---
+++ Assignment6.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    class Television
+ { 
+     private int channel = 0; 
+     private int volume = 0; 
+     private bool isOn = false; 
+ 
+
+     //property for IsOn 
+     public bool IsOn
+     { 
+         get 
+         { 
+             return isOn; 
+         } 
+         set 
+         { 
+             isOn = value; 
+         } 
+ 
+
+     } 
+ 
+
+     //property for Channel 
+     public int Channel
+     { 
+         get 
+         { 
+             return channel; 
+         } 
+         set 
+         { 
+             channel = value; 
+         } 
+ 
+
+     } 
+ 
+
+     //property for Volume 
+     public int Volume
+     { 
+         get 
+         { 
+             return volume; 
+         } 
+         set 
+         { 
+             volume = value; 
+         } 
+
+
+     } 
+ } 
+ 
+
+ class TestTV
+ { 
+     static void Main()
+     { 
+         Television tv = new Television(); 
+ 
+
+         if (!tv.IsOn)//getting 
+         { 
+             tv.IsOn = true; // setting 
+         } 
+ 
+
+         tv.Channel = 3; //setting 
+ 
+
+         tv.Volume++; //gets and sets it 
+         tv.Volume++; 
+         tv.Volume++; 
+         tv.Volume++; 
+ 
+
+         tv.IsOn = false; //setting 
+     } 
+ } 
+}
